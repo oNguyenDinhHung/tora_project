ae85db09c0015389f7bd004ffe2b32cb97848d9f
huntojkojqw01 <hungnd.k58@gmail.com>
2017-10-07 13:59:23 +0700

    them header cho website va sua giao dien cua devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
-Rails.application.routes.draw do
-  devise_for :users
+Rails.application.routes.draw do  
+  devise_for :users, controllers: {
+        registrations: 'users/registrations'
+      }
   get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
