590faa8eef226168d7795715ff7f3d665298bb44
hungnd <hungnd.k58@gmail.com>
2017-10-09 17:48:52 +0700

    chay actioncable thanh cong tren production

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'devise'
 gem 'passenger'
 gem 'bootstrap'
 gem 'jquery-rails'
+gem "redis", "~> 3.0" 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'home/index'
   post 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"  
+  root to: "home#index"
+  mount ActionCable.server => '/cable'  
 end
