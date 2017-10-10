316934b20d41b536067447137d929be939ae6b8e
hungnd <hungnd.k58@gmail.com>
2017-10-10 17:18:58 +0700

    tao file seed du lieu cho bang Maker and Store

--- app/models/app.rb
+++ app/models/app.rb
@@ -2,4 +2,5 @@ class App < ApplicationRecord
   belongs_to :maker
   belongs_to :source
   belongs_to :keyword
+  validates :name,presence: true,uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Store.destroy_all
+Store.create(name: "MediaMart")
+Store.create(name: "BigC")
+Store.create(name: "Tikitaka")
+Store.create(name: "Sendo")
+Store.create(name: "Nguyen Kim")
+
+Maker.destroy_all
+Maker.create(name: "Toyota")
+Maker.create(name: "Honda")
+Maker.create(name: "Yamaha")
+Maker.create(name: "Apple")
+Maker.create(name: "Microsoft")
+Maker.create(name: "Samsung")
+
