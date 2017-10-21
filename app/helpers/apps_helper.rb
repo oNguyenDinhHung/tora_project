904dc5f1d4fb2457f74233740fd4bab267d88685
huntojkojqw01 <hungnd.k58@gmail.com>
2017-10-21 15:37:58 +0700

    use star_rating_for app.rating in homepage

--- app/helpers/apps_helper.rb
+++ app/helpers/apps_helper.rb
@@ -1,2 +1,16 @@
 module AppsHelper
+	def star_rating_for value		
+		if value
+			html="<div style='color: yellow;'>"
+			value=(value.round(1)*2).round/2.0
+			while value>0.5
+				value-=1.0
+				html+="<span class='fa fa-star'></span>"
+			end
+			html+="<span class='fa fa-star-half-o'></span>" if value>0
+			raw(html+'</div>')
+		else
+			raw t('views.app.no_review')
+		end				
+	end
 end
