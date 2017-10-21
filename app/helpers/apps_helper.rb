d49f9874ec3a164788a36e564bbdc397ae8679d0
huntojkojqw01 <hungnd.k58@gmail.com>
2017-10-21 18:57:30 +0700

    sort app by rating and review number

--- app/helpers/apps_helper.rb
+++ app/helpers/apps_helper.rb
@@ -1,14 +1,14 @@
 module AppsHelper
 	def star_rating_for value		
 		if value
-			html="<div style='color: yellow;'>"
+			html="<span style='color: yellow;'>"
 			value=(value.round(1)*2).round/2.0
 			while value>0.5
 				value-=1.0
 				html+="<span class='fa fa-star'></span>"
 			end
 			html+="<span class='fa fa-star-half-o'></span>" if value>0
-			raw(html+'</div>')
+			raw(html+'</span>')
 		else
 			raw t('views.app.no_review')
 		end				
