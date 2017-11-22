8e511cd875ab2d6dbae19d7548d1096ffa8ec5bf
huntojkojqw01 <hungnd.k58@gmail.com>
2017-11-22 17:03:20 +0700

    hotfix link edit app

--- db/schema.rb
+++ db/schema.rb
@@ -69,6 +69,14 @@ ActiveRecord::Schema.define(version: 20171112075558) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.integer "app_id"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["app_id"], name: "index_photos_on_app_id"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.integer "user_id"
     t.integer "app_id"
